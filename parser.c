de029c78c653c6157fe5873e4b092ee385211858
Matej Alexej Helc <73157309+MatejHelc@users.noreply.github.com>
2021-11-26 02:37:31 +0100

    Added new functions (still not working)

--- parser.c
+++ parser.c
@@ -1,6 +1,6 @@
 /**
  * Implementace překladače imperativního jazyka IFJ21.
- *
+ * 
  * @brief Syntactic and semantic check header
  * @author Matej Alexej Helc
  */
@@ -17,12 +17,12 @@ int SCANNER_RESPONSE;   // for return value from get_token()
 int PARAMETERS_NUMBER;
 token_struct *token;
 table_t *table;
-tab_item_t *tableItem;    //todo zmena
+table_item *table_item;
 /*-----------------*/
 
 /** Pravidlá
     <header>    -> require "ifj21" <func>
-    <func>      -> function id ( <params> ) <rets> <body> end <program>
+ !<func>      -> function id ( <params> ) <rets> <body> end <program>
     <params>    -> ε
     <params>    -> id : <data_type> <param>
     <param>     -> ε
@@ -32,23 +32,23 @@ tab_item_t *tableItem;    //todo zmena
     <def_var>   -> ε
     <state_l>   -> <comm> <state_l>
     <state_l>   -> ε
-    <comm>      -> id <assign>
-    <comm>      -> if <condition> then <state_l> else <stale_l> end
-    <comm>      -> while <condition> do <state_l> end
-    <comm>      -> id ( <params_in> )
-    <comm>      -> return <param_in>
-    <comm>      -> reads ()
-    <comm>      -> readi ()
-    <comm>      -> readn ()
-    <comm>      -> write (<params_in>)
-    <comm>      -> tointeger (<expression>)
-    <comm>      -> substr ()            /////
-    <comm>      -> ord ()                /////
-    <comm>      -> chr ()                /////
-    <params_in> -> ε
-    <params_in> -> id <param_in>
-    <param_in>  -> , id <param_in>
-    <param_in>  -> ε
+ !<comm>      -> id <assign>
+ !<comm>      -> if <condition> then <state_l> else <stale_l> end
+ !<comm>      -> while <condition> do <state_l> end
+ !<comm>      -> id ( <params_in> )
+ !<comm>      -> return <param_in>
+ !<comm>      -> reads ()
+ !<comm>      -> readi ()
+ !<comm>      -> readn ()
+ !<comm>      -> write (<params_in>)
+ !<comm>      -> tointeger (<expression>)
+ !<comm>      -> substr ()            /////
+ !<comm>      -> ord ()                /////
+ !<comm>      -> chr ()                /////
+ !<params_in> -> ε
+ !<params_in> -> id <param_in>
+ !<param_in>  -> , id <param_in>
+ !<param_in>  -> ε
     <rets>      -> : <data_type> <ret>
     <rets>      -> ε
     <ret>       -> , <data_type> <ret>
@@ -57,8 +57,8 @@ tab_item_t *tableItem;    //todo zmena
     <data_type> -> number
     <data_type> -> string
     <data_type> -> nil
-    <assign>    -> = <expression>
-    <assign>    -> ε
+ !<assign>    -> = <expression>
+ !<assign>    -> ε
  */
 
 
@@ -72,7 +72,7 @@ int get_token_check() {
     while (true) {
         if (token->attribute.keyword == KEYWORD_REQUIRE) {
             SCANNER_RESPONSE = get_token(token);
-            if (token->type == TOKEN_ID && !strcmp((const char *)token->attribute.string, "\"ifj21\"")) // error: incompatible type for argument 1 of ‘strcmp’, fixed, but not 100% sure
+            if (token->type == TOKEN_ID && !strcmp(*token->attribute.string, "\"ifj21\""))
                 break;
         }
     }
@@ -83,50 +83,64 @@ int get_token_check() {
  *
  */
 int p_function() {
-    int function;
     init_hashtable(table);
-    SCANNER_RESPONSE = get_token(token);
-    if (token->attribute.keyword != KEYWORD_FUNCTION)  //missing keyword function
-        return ERR_SYNTAX;
+    int function;
+    SCANNER_RESPONSE = get_token(token));
+    while (1) {
+        if (token->attribute.keyword != KEYWORD_FUNCTION)  //missing keyword function
+            return ERR_SYNTAX;
 
-    SCANNER_RESPONSE = get_token(token);
-    if (token->type != TOKEN_ID)
-        return ERR_SYNTAX;
+        SCANNER_RESPONSE = get_token(token));
+        if (token->type != TOKEN_ID)
+            return ERR_SYNTAX;
 
-    // error: incompatible type for argument 1 of ‘strcmp’
-    // if (!strcmp(*token->attribute.string, "main") || !strcmp(*token->attribute.string, "write") ||
-    //     !strcmp(*token->attribute.string, "reads") || !strcmp(*token->attribute.string, "readn") ||
-    //     !strcmp(*token->attribute.string, "readi") || !strcmp(*token->attribute.string, "tointeger") ||
-    //     !strcmp(*token->attribute.string, "substr") || !strcmp(*token->attribute.string, "ord") ||
-    //     !strcmp(*token->attribute.string, "chr"))
-    //     return ERR_UNDEF;
+        if (!strcmp(*token->attribute.string, "main") || !strcmp(*token->attribute.string, "write") ||
+            !strcmp(*token->attribute.string, "reads") || !strcmp(*token->attribute.string, "readn") ||
+            !strcmp(*token->attribute.string, "readi") || !strcmp(*token->attribute.string, "tointeger") ||
+            !strcmp(*token->attribute.string, "substr") || !strcmp(*token->attribute.string, "ord") ||
+            !strcmp(*token->attribute.string, "chr"))
+            return ERR_UNDEF;
 
-    // if (/*TODO porovnanie ci sa ID nezhoduje s nazvom nejakej funkcie*/)
-    //     return ERR_UNDEF;
+        if (search_hashtable(table, *token->attribute.string) != NULL) {
+            delete_all_hashtable(table);
+            return ERR_UNDEF;
+        }
 
-    // if (tableItem /*TODO insert id (key) do tabulky)*/) {
-    //     /*error ak sa nepodarilo nahrat do tabulky */
-    // }
+        if ((table_item = insert_element_hashtable(table, token->attribute.string->string)) == NULL) {
+            delete_all_hashtable(table);
+            return ERR_INTERNAL;
+        }
 
-    SCANNER_RESPONSE = get_token(token);
-    if (token->type != TOKEN_BRACKET_ROUND_L)
-        return ERR_SYNTAX;
+        SCANNER_RESPONSE = get_token(token);
+        if (token->type != TOKEN_BRACKET_ROUND_L) {
+            delete_all_hashtable(table);
+            return ERR_SYNTAX;
+        }
 
-    function = p_params();
-    if (function != OK)
-        return function;
-
-    SCANNER_RESPONSE = get_token(token);
-    if (token->type == TOKEN_DDOT) {
-        function = p_rets();
+        function = p_params();
         if (function != OK)
             return function;
+
+        SCANNER_RESPONSE = get_token(token);
+        if (token->type == TOKEN_DDOT) {
+            function = p_rets();
+            if (function != OK)
+                return function;
+        }
+
+        funtion = p_body();
+        if (function != OK)
+            return function;
+
+        SCANNER_RESPONSE = get_token(token);
+        if (token->attribute.keyword != KEYWORD_END) {
+            delete_all_hashtable(table);
+            return ERR_SYNTAX;
+        }
+        SCANNER_RESPONSE = get_token(token));   //not EOF, loading next function
+        if (token->type == TOKEN_EOF)
+            return OK;
     }
-
-    function = p_body();
-    if (function != OK)
-        return function;
-
 }
 
 /** <params>    -> ε
@@ -136,22 +150,27 @@ int p_function() {
  *
  */
 int p_params() {
+    table_item *params;
     SCANNER_RESPONSE = get_token(token);
     if (token->type != TOKEN_BRACKET_ROUND_R)  //<params>    -> ε
         return OK;
 
+    int i = 0;
     while (1) {
-        if (token->type != TOKEN_ID)
-            /* TODO uvolnit tabulku*/
+        if (token->type != TOKEN_ID) {
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
+        }
 
-        // if (/*TODO porovnanie ci sa ID nezhoduje s nazvom nejakej funkcie*/)
-        //     return ERR_UNDEF;
-        /*TODO vlozit ID do tabulky*/
+        if ((params = search_hashtable(table, table_item->key)) == NULL) {
+            delete_all_hashtable(table);
+            return ERR_INTERNAL;
+        }
+        table_item->data.type_parameter_values[i] = token->attribute.string->string;    //TODO
 
         SCANNER_RESPONSE = get_token(token);
         if (token->type != TOKEN_DDOT) {
-            /* TODO uvolnit tabulku*/
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
         }
 
@@ -159,22 +178,25 @@ int p_params() {
         if (token->attribute.keyword != KEYWORD_INTEGER &&
             token->attribute.keyword != KEYWORD_NUMBER &&
             token->attribute.keyword != KEYWORD_STRING) {
-            /* TODO uvolnit tabulku*/
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
         }
-
-        /*TODO vlozit d.typ ID do tabulky */
-
+        /*TODO dát. typ parametru*/
         SCANNER_RESPONSE = get_token(token);
         if (token->type != TOKEN_BRACKET_ROUND_R)
             return OK;
 
         if (token->type != TOKEN_COMMA) {
-            /* TODO uvolnit tabulku*/
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
         }
 
         SCANNER_RESPONSE = get_token(token);
+        if (i > MAX_PARAMETERS) {
+            delete_all_hashtable(table);
+            return ERR_PARAM;
+        }
+        i++;
     }
 }
 /** <rets>      -> : <data_type> <ret>
@@ -184,26 +206,38 @@ int p_params() {
  *
  */
 int p_rets() {
+    table_item *returns;
+    int i = 0;
     while (1) {
         SCANNER_RESPONSE = get_token(token);
         if (token->attribute.keyword != KEYWORD_INTEGER &&
             token->attribute.keyword != KEYWORD_NUMBER &&
             token->attribute.keyword != KEYWORD_STRING) {
-            /* TODO uvolnit tabulku*/
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
         }
 
-        /*TODO vlozit d.typ ID do tabulky */
+        if ((returns = search_hashtable(table, table_item->key)) == NULL) {
+            delete_all_hashtable(table);
+            return ERR_INTERNAL;
+        }
+        table_item->data.type_return_values[i] = token->attribute.string->string;   //TODO
 
         SCANNER_RESPONSE = get_token(token);
         if (token->type != TOKEN_COMMA) {
-            /* TODO uvolnit tabulku*/
+            delete_all_hashtable(table);
             return ERR_SYNTAX;
         }
         if (token->type == TOKEN_EOL)
             return OK;
 
         SCANNER_RESPONSE = get_token(token);
+
+        if (i > MAX_RETURN_TYPES) {
+            delete_all_hashtable(table);
+            return ERR_PARAM;
+        }
+        i++;
     }
 }
 
@@ -217,12 +251,16 @@ int p_body(){
         SCANNER_RESPONSE = get_token(token);
         if (token->attribute.keyword == KEYWORD_LOCAL) {
             function = p_defvar();
-            if (function != OK)
+            if (function != OK) {
+                delete_all_hashtable(table);
                 return function;
+            }
         }
         function = p_state_l();
-        if (function != OK)
+        if (function != OK) {
+            delete_all_hashtable(table);
             return function;
+        }
     }
 }
 /** <def_var>   -> local id : <data_type> <assign> <def_var>
@@ -233,14 +271,15 @@ int p_defvar(){
     int function;
     SCANNER_RESPONSE = get_token(token);
     if (token->type != TOKEN_ID) {
-        /* TODO uvolnit tabulku*/
+        delete_all_hashtable(table);
         return ERR_SYNTAX;
     }
-    /* TODO nahrat ID do tabulky*/
+
+    /* TODO Lokálne premenné kam?*/
 
     SCANNER_RESPONSE = get_token(token);
     if (token->type != TOKEN_DDOT) {
-        /* TODO uvolnit tabulku*/
+        delete_all_hashtable(table);
         return ERR_SYNTAX;
     }
 
@@ -248,45 +287,122 @@ int p_defvar(){
     if (token->attribute.keyword != KEYWORD_INTEGER &&
         token->attribute.keyword != KEYWORD_NUMBER &&
         token->attribute.keyword != KEYWORD_STRING) {
-        /* TODO uvolnit tabulku*/
+        delete_all_hashtable(table);
         return ERR_SYNTAX;
     }
-    /* TODO nahrat hodnotu do tabulky*/
+
+    /* TODO Lokálne premenné dát. typ*/
 
     SCANNER_RESPONSE = get_token(token);
     if (token->type == TOKEN_ASSIGN){
         function = p_assign();
         return function;
-    } else if (token->type != TOKEN_EOL){
-        return ERR_SYNTAX;
     }
-    return OK;
+    if (token->type == TOKEN_EOL) {
+        SCANNER_RESPONSE = get_token(token);
+        return OK;
+    }
+
+    return ERR_SYNTAX;
 }
-/** TODO
+/** <assign>    -> = <expression>
+ *  <assign>    -> ε
  *
  *
  */
 int p_assign(){
     int function;
     SCANNER_RESPONSE = get_token(token);
-    /*TODO */
+    if (token->type != TOKEN_ID && token->type != TOKEN_INT &&
+        token->type != TOKEN_DOUBLE && token->type != TOKEN_STRING) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    /*TODO vložiť hodnotu do premennej*/
 }
 
 /** <state_l>   -> <comm> <state_l>
  *  <state_l>   -> ε
  *
  */
- int p_state_l(){
-     int function;
+int p_state_l() {
+    int function;
 
-     switch (token->attribute.keyword) {    //switch zbytočný prerobiť na if else
-         case KEYWORD_WHILE:
-             break;
-         case KEYWORD_IF:
-             break;
-         default:
-             break;
-          /* TODO */
-     }
+    if (token->attribute.keyword == KEYWORD_WHILE) {
+        function = p_while();
+        return function;
+    } else if (token->attribute.keyword == KEYWORD_IF) {
+        function = p_if();
+        return function;
+    } else if (token->type == TOKEN_ID) {
 
- }
+    } else if (token->attribute.keyword == KEYWORD_RETURN) {
+        function = p_params_in();
+        return function;
+    } else if (token->type == TOKEN_EOL) {
+        return OK;
+    }
+
+}
+
+int p_while(){
+    int function;
+
+    //čakám nejakú podmienku example ( i < y )
+    /* TODO WHILE*/
+    SCANNER_RESPONSE = get_token(token);
+    if (token->attribute.keyword != KEYWORD_DO) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    SCANNER_RESPONSE = get_token(token);
+    function = p_state_l();
+    if (function != OK) {
+        delete_all_hashtable(table);
+        return function;
+    }
+
+    if (token->attribute.keyword != KEYWORD_END) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    return OK;
+}
+
+int p_if(){
+    int function;
+    //čakám nejakú podmienku example ( i < y )
+    /* TODO IF */
+    SCANNER_RESPONSE = get_token(token);
+    if (token->attribute.keyword != KEYWORD_THEN) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    SCANNER_RESPONSE = get_token(token);
+    function = p_state_l();
+    if (function != OK) {
+        delete_all_hashtable(table);
+        return function;
+    }
+    SCANNER_RESPONSE = get_token(token);
+    if (token->attribute.keyword != KEYWORD_ELSE) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    SCANNER_RESPONSE = get_token(token);
+    function = p_state_l();
+    if (function != OK) {
+        delete_all_hashtable(table);
+        return function;
+    }
+    SCANNER_RESPONSE = get_token(token);
+    if (token->attribute.keyword != KEYWORD_END) {
+        delete_all_hashtable(table);
+        return ERR_SYNTAX;
+    }
+    return OK;
+}
+
+int p_params_in(){
+    /* TODO PARAMS_IN */
+}
