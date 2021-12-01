dfaa945f5e14f7c8a599183ec0634be82aadc5fd
Matej Alexej Helc <73157309+MatejHelc@users.noreply.github.com>
2021-12-01 16:53:04 +0100

    Grammar update, added funtion_parser()

--- parser.c
+++ parser.c
@@ -22,10 +22,10 @@ token->attribute.string->string);                               \
 if (item == NULL) return ERR_INTERNAL
 
 // macro that we use for searching item in table
-#define SEARCH_ITEM(_STRING)                                    \
-tab_item_t *item = search_hashtable(stack->top->table,          \
-(_STRING));                                                     \
-if (item == NULL) return ERR_INTERNAL
+#define SEARCH_ITEM(_TABLE, _STRING)                            \
+tab_item_t *item = search_hashtable((_TABLE), (_STRING));       \
+bool is_there = false;                                          \
+if (item != NULL) is_there = true;
 
 // macro that we use for check if keyword of token is type
 #define IS_TYPE                                                 \
@@ -35,10 +35,40 @@ if (item == NULL) return ERR_INTERNAL
 (token->attribute.keyword == KEYWORD_STRING)  ||                \
 (token->attribute.keyword == KEYWORD_NIL))
 
-//
+// macro that we use for check if keyword is function
 #define IS_FUNCTION                                             \
 (token->type == TOKEN_KEYWORD) &&                               \
 (token->attribute.keyword == KEYWORD_FUNCTION)
+
+// macro we need for allocate memory
+#define ALLOC                                                   \
+token = (token_struct *)malloc(sizeof(token_struct));           \
+stack = (data_stack_t *)malloc(sizeof(data_stack_t));           \
+init_data_stack(stack);                                         \
+if (!(string_init(&string))) return ERR_INTERNAL;               \
+define_working_str(&string)
+
+// macro we need for memory free
+#define CLEAN                                                   \
+string_free(&string);                                           \
+empty_data_stack(stack);                                        \
+free(token);                                                    \
+free(stack)
+
+// macro that we use for check if token is ID
+#define IS_ID (token->type == TOKEN_ID)
+
+// macro that we use for check if token string is build-in func string
+#define IS_BUILT_IN_FUNCTION \
+IS_ID                                                   &&      \
+((!strcmp(token->attribute.string->string, "readi"))    ||      \
+(!strcmp(token->attribute.string->string, "reads"))     ||      \
+(!strcmp(token->attribute.string->string, "readn"))     ||      \
+(!strcmp(token->attribute.string->string, "substr"))    ||      \
+(!strcmp(token->attribute.string->string, "tointeger")) ||      \
+(!strcmp(token->attribute.string->string, "ord"))       ||      \
+(!strcmp(token->attribute.string->string, "chr"))       ||      \
+(!strcmp(token->attribute.string->string, "write")))
 
 /* Global variables*/
 int SCANNER_RESPONSE;       // for return value from get_token()
@@ -49,12 +79,12 @@ data_stack_t *stack;        // stack
 
 /** Pravidlá
     <header>        -> require "ifj21" <program>
-    <program>       -> global id : function(<params_g>) <rets> <program>
+    <program>       -> global id : function ( <params_g> ) <rets> <program>
     <program>       -> function id ( <params> ) <rets> <state_l> end <program>
-    <program>       -> id (<params_in>) <program>
+    <program>       -> <call> <program>
     <program>       -> ε
     <params_g>      -> <data_type> <param_g>
-    <pramms_g>      -> ε
+    <params_g>      -> ε
     <param_g>       -> , <data_type> <param_g>
     <param_g>       -> ε
     <params>        -> ε
@@ -69,12 +99,14 @@ data_stack_t *stack;        // stack
     <comm>          -> id <assign>
     <comm>          -> if <conditions> then <state_l> else <stale_l> end
     <comm>          -> while <conditions> do <state_l> end
-    <comm>          -> id ( <expressions> )
+    <comm>          -> <call>
     <comm>          -> return <expressions>
-    <comm>          -> write (<expressions>)
+    <comm>          -> write ( <expressions> )
+    <call>          -> id ( <expressions> )
+    <call>          -> ε
     <params_in>     -> ε
-    <params_in>     -> id <param_in>
-    <param_in>      -> , id <param_in>
+    <params_in>     -> expression <param_in>
+    <param_in>      -> , expression <param_in>
     <param_in>      -> ε
     <rets>          -> : <data_type> <ret>
     <rets>          -> ε
@@ -84,8 +116,9 @@ data_stack_t *stack;        // stack
     <data_type>     -> number
     <data_type>     -> string
     <data_type>     -> nil
-    <assign>        -> = <expressions>
+    <assign>        -> = <assigns>
     <assign>        -> ε
+    <assigns>       -> <call> <expressions>
     <expressions>   -> expression <expression>
     <expression>    -> , expression <expression>
     <expression>    -> ε
@@ -161,8 +194,86 @@ int start_program_parser() {
  * <func>      -> function id ( <params> ) <rets> <body> end <program>
  * */
 int function_parser() {
-    printf("i love u so much!\n");
-    return 0;
+    GET_TOKEN;
+    if (IS_ID) {    // (function) ID
+        if (IS_BUILT_IN_FUNCTION) return ERR_UNDEF;
+        SEARCH_ITEM(stack->top->table, token->attribute.string->string);
+        if (is_there) {
+            if (item->data->defined) {
+                return ERR_UNDEF;
+            } else {
+                item->data->defined = true;
+            }
+        }
+        if (!push_data_item(stack)) return ERR_INTERNAL;
+
+        GET_TOKEN;
+        if (token->type != TOKEN_BRACKET_ROUND_L) return ERR_SYNTAX;
+
+        GET_TOKEN;
+        for (int i = 0; token->type != TOKEN_BRACKET_ROUND_R; ++i) {    //<params> maybe new func. ?
+            if (token->type == TOKEN_EOL) {
+                i--;
+            } else if ((i % 2) == 0) {
+                if (IS_ID) {
+                    if (IS_BUILT_IN_FUNCTION) return ERR_UNDEF;
+                    if (!(TABLE_ITEM = insert_element_hashtable(stack->top->table, token->attribute.string->string)) return ERR_INTERNAL; /*TODO*/
+
+                    GET_TOKEN;
+                    if (token->type != TOKEN_DDOT) return ERR_SYNTAX;
+
+                    GET_TOKEN;
+                    if (IS_TYPE) {
+                        if (token->attribute.keyword == KEYWORD_INTEGER)
+                            item->data->type_parameter_values[(int) (i / 2)] = TYPE_INTEGER;
+                        else if (token->attribute.keyword == KEYWORD_NUMBER)
+                            item->data->type_parameter_values[(int) (i / 2)] = TYPE_DOUBLE;
+                        else if (token->attribute.keyword == KEYWORD_STRING)
+                            item->data->type_parameter_values[(int) (i / 2)] = TYPE_STRING;
+                        else
+                            item->data->type_parameter_values[(int) (i / 2)] = TYPE_NULL;
+                    } else {
+                        return ERR_SYNTAX;
+                    }
+                } else {
+                    return ERR_SYNTAX;
+                }
+            } else {
+                if (token->type != TOKEN_COMMA) return ERR_SYNTAX;
+            }
+            GET_TOKEN;
+            }
+        GET_TOKEN;
+        if (token->type == TOKEN_DDOT) {    // <rets> maybe new func. ?
+            GET_TOKEN;
+            int i = 0;
+            while (true) {
+                if (IS_TYPE) {
+                    if (token->attribute.keyword == KEYWORD_INTEGER)
+                        item->data->type_parameter_values[i] = TYPE_INTEGER;
+                    else if (token->attribute.keyword == KEYWORD_NUMBER)
+                        item->data->type_parameter_values[i] = TYPE_DOUBLE;
+                    else if (token->attribute.keyword == KEYWORD_STRING)
+                        item->data->type_parameter_values[i] = TYPE_STRING;
+                    else
+                        item->data->type_parameter_values[i] = TYPE_NULL;
+                } else {
+                    return ERR_SYNTAX;
+                }
+                GET_TOKEN;
+                if (token->type == TOKEN_COMMA) {
+                    i++;
+                    GET_TOKEN;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+        /*TODO <body> end*/
+    } else {
+        return ERR_SYNTAX;
+    }
 }
 
 /**
