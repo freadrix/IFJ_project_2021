23ca7e59a5444f2e7d859d6a85f0f6809ccd0ad4
Verevkin-A <sasha.verevkin1993@mail.ru>
2021-12-09 03:16:44 +0100

    sads

--- parser.c
+++ parser.c
@@ -1155,6 +1155,5 @@ int write_func_parser() {
         }
     }
     if ((i > 0) && (i % 2 == 0)) return ERR_SYNTAX;
-//    if(!code_generate_write_function((int) (i/2)), "ssss") return ERR_INTERNAL;
     return OK;
 }
